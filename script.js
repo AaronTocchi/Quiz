09f8f119c222ed4c679f741edad4336bad2319d9
Toki <actocchi@gmail.com>
2019-09-23 18:56:27 -0700

    html structure built

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-//  make global variabls for questions
+//  make global variables for questions
 //  
+
+// array of questions to pull from
+let questions = [
+    {
+        title: "_____ loop one of the most common loops; is used to check for certain conditions and then repeatedly execute a block of code as long as those conditions are met.",
+        choices: ["Four", "For", "Loop D", "conditional"],
+        answer: "For"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "An Array's index begins at what numerical value"
+        choices: ["0","-1","1","Where ever you want it to"],
+        answer: "0"
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+    {
+        title:
+        choices:
+        answer:
+    },
+]
