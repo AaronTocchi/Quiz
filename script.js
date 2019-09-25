781b874e91a9be2f97e20493afe6a85e854e5e31
Toki <actocchi@gmail.com>
2019-09-24 18:27:08 -0700

    functional timer and cleared up bootstrap and jquery sources

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 //  make global variables for questions
+// create timer
+// create loop for questions
 //  on start button hide starting-paragraph, start timer, and show questions
 //  questions shows a question from the array each with their own listed buttons
-//  maybe make a for loop for questions and use return so it only progresses once each time
 //  on choices: if correct add point, show correct on feedback, and go to next question. 
 //  on choices: Else subtract 10 seconds from time, show wrong on feedback and got to next question.
 //  repeat until moved through array
@@ -11,7 +12,7 @@
 // array of questions to pull from
 let questions = [
     {
-        title: "_____ loop one of the most common loops; is used to check for certain conditions and then repeatedly execute a block of code as long as those conditions are met.",
+        title: "_____ loop, one of the most common loops; is used to check for certain conditions and then repeatedly execute a block of code as long as those conditions are met.",
         choices: ["Four", "For", "Loop D", "conditional"],
         answer: "For"
     },
@@ -35,4 +36,27 @@ let questions = [
         choices: ["functions","arrays", "objects", "coffee :'("],
         answer: "coffee :'("
     },
-]
+];
+
+// Start timer function
+function startTimer(duration, display) {
+    let timer = duration, seconds;
+    setInterval(function () {
+        seconds = parseInt(timer % 60, 10);
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+        display.textContent =":" + seconds;
+
+        if (--timer === 0) {
+            clearInterval(timer);
+        }
+    }, 1000);
+}
+
+    // hide starting-paragraph
+    $("#start").on("click", function(){
+        $(".start").addClass("d-none");
+        let oneMinute = 60,
+        display = document.querySelector('#time');
+    startTimer(oneMinute, display); 
+    
+    })
