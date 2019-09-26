4a32e2990902b5daea3dcaec7eef1d0238e6af9b
Toki <actocchi@gmail.com>
2019-09-26 09:44:29 -0700

    timer decrement working and score

--- script.js
+++ script.js
@@ -32,8 +32,8 @@ let questions = [
         answer: 2
     },
     {
-        title:" Javescript DOES NOT have____",
-        choice: [" functions"," arrays", " objects", " coffee :'("],
+        title: " Javescript DOES NOT have____",
+        choice: [" functions", " arrays", " objects", " coffee :'("],
         answer: 3
     },
 ];
@@ -43,11 +43,12 @@ let oneMinute = 60;
 let currentIndex = 0; // keeps track of current question
 let questionLength;
 let gameTimer;
-let players; 
+let players;
 
 
 // Start timer function
-function startTimer(oneMinute, display) {
+function startTimer() {
+    clearInterval(gameTimer);
     gameTimer = setInterval(function () {
         oneMinute--;
         if (oneMinute === 0) {
@@ -60,112 +61,120 @@ function startTimer(oneMinute, display) {
 function saveScore() {
     localStorage.setItem("display", JSON.stringify(display));
 };
+
 // on game end stop timer, hide questions,  save score
-function gameEnd(){
+function gameEnd() {
+    console.log('Inside gameEnd function', display, oneMinute);
     clearInterval(gameTimer);
     $("#questions").attr("class", "hide")
     $("#scoring-screen").attr("class", "jumbotron");
-    $("#end-score").append(display);
+    $("#end-score").append(oneMinute);
+    display.textContent = oneMinute;
 }
 
 
-     // function for on button click
-     $("#questions").on("click", '.button', function(){
-         console.log(this);
-         let answer = parseInt($(this).attr('data-value'));
-        console.log(answer);
-        // if correct answer, then diplay "Correct"
-        // increment currentIndex by 1
-        // then displayQuestion()
+// function for on button click
+$("#questions").on("click", '.button', function () {
+    console.log(this);
+    let answer = parseInt($(this).attr('data-value'));
+    console.log(answer);
+    // if correct answer, then diplay "Correct"
+    // increment currentIndex by 1
+    // then displayQuestion()
 
-        // if incorrect, display "Incorrect"
-        // subtract 10 from total time (oneMinute)
-        // incremember currentIndex by1
-        // displayQuestion()
+    // if incorrect, display "Incorrect"
+    // subtract 10 from total time (oneMinute)
+    // incremember currentIndex by1
+    // displayQuestion()
 
-        // *** But include some condition that checks if currentIndex === questionsLength
-        // if yes, endGame
+    // *** But include some condition that checks if currentIndex === questionsLength
+    // if yes, endGame
 
-        if (answer === questions[currentIndex].answer) {
-            ++currentIndex;
-           $("#question-title").empty();
-           $("#choices").empty();
-            displayQuestion();
-            console.log(currentIndex);
-            
-            
-        }
-        else if (answer !=questions[currentIndex].answer) {
-              gameTimer-=10000;
-            //  ++currentIndex;
-            //  $("#question-title").empty();
-            //  $("#choices").empty();
-            //   displayQuestion(); 
-
-        }
-       
-     });
-
-     function displayQuestion() {
-        
-         titles = questions[currentIndex].title;
-        choices = questions[currentIndex].choice; // array
-        
-
-        
-       
-       
-    //    show question title
-        $("#question-title").append(titles);
-        $("#questions").attr("class", "jumbotron");
-   
-    //  show choices and make buttons loop through for buttons
-      
-        for (let i = 0; i < choices.length; i++){
-            let choiceButton = $("<button>" + choices[i] + "</button>");
-            choiceButton.addClass("button");
-            choiceButton.attr('data-value', i)
-            $("#choices").append(choiceButton);
-        }
-        if (currentIndex === 4) {
-            gameEnd();
-        }
-     }
-
-    // hide starting-paragraph and start timer
-    $("#start").on("click", function(){
-        event.preventDefault();
-        $(".start").addClass("d-none");
-        display.textContent = oneMinute;
-        startTimer(oneMinute, display); 
+    if (answer === questions[currentIndex].answer) {
+        console.log('Correct aswer');
+        ++currentIndex;
+        $("#question-title").empty();
+        $("#choices").empty();
         displayQuestion();
-        
-    })
 
-    $("#submit").on("click", function(){
-        window.location.href="highscore.html";
-        saveScore();
 
-    })
+
+    } else if (answer !== questions[currentIndex].answer) {
+        console.log('Wrong answer');
+        oneMinute -= 10;
+        console.log(oneMinute);
+        startTimer();
+         ++currentIndex;
+         $("#question-title").empty();
+         $("#choices").empty();
+          displayQuestion(); 
+
+    }
+
+    if (currentIndex === 4) {
+        gameEnd();
+    }
+
+
+});
+
+function displayQuestion() {
+
+    titles = questions[currentIndex].title;
+    choices = questions[currentIndex].choice; // array
+
+
+
+
+
+    //    show question title
+    $("#question-title").append(titles);
+    $("#questions").attr("class", "jumbotron");
+
+    //  show choices and make buttons loop through for buttons
+
+    for (let i = 0; i < choices.length; i++) {
+        let choiceButton = $("<button>" + choices[i] + "</button>");
+        choiceButton.addClass("button");
+        choiceButton.attr('data-value', i)
+        $("#choices").append(choiceButton);
+    }
+}
+
+// hide starting-paragraph and start timer
+$("#start").on("click", function () {
+    event.preventDefault();
+    $(".start").addClass("d-none");
+    display.textContent = oneMinute;
+    startTimer();
+    displayQuestion();
+
+})
+// submit button
+$("#submit").on("click", function () {
+    window.location.href = "highscore.html";
+    saveScore();
+
+})
         /// CHECKPOINT: CONFIRMED ///
 
 
         /// OLD CODE ///
         // for loop for the questions    
     // for (currentIndex = 0; currentIndex < questions.length; currentIndex++){
-        
+
     //    isolated the variables
     //     let titles = questions[currentIndex].title;
     //     let choices = questions[currentIndex].choice; // array
     //     let answers = questions[currentIndex].answer;
-       
-       
+
+
     // //    show question title
     //     $("#question-title").append(titles);
     //     $("#questions").attr("class", "jumbotron");
-   
+
     // //  show choices and make buttons loop through for buttons
-      
+
     //     for (let i = 0; i < choices.length; i++){
     //         let choiceButton = $("<button>" + choices[i] + "</button>");
     //         choiceButton.addClass("button");
@@ -179,8 +188,8 @@ function gameEnd(){
         //Choose answer based on event listner
         //create function check answer
         // 
-          
-          
-    
-    
+
+
+
+
 
